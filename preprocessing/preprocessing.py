173549f6b1711f8cc8ba81fc71a06b8df969476b
Clément Boulay <clement.boulay@student-cs.fr>
2022-10-14 10:53:14 +0200

    Fix preprocessing removing the test csv

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -3,14 +3,16 @@ Preprocessing.py
 """
 
 # Python ≥3.5 is required
+from audioop import minmax
 import sys
-from tkinter import Y
 assert sys.version_info >= (3, 5)
 import os
 
 # Scikit-Learn ≥0.20 is required
 import sklearn
 assert sklearn.__version__ >= "0.20"
+
+from sklearn.model_selection import train_test_split
 
 # Numpy arrays are used to store training and test data.
 import numpy as np
@@ -31,40 +33,50 @@ def load_data():
     TEST_CSV = "test_airbnb_berlin.csv"
 
     # Load training data
-    training_data = pd.read_csv(os.path.join(
+    df = pd.read_csv(os.path.join(
                     os.path.dirname(os.getcwd()),
                     DATA_FOLDER,
                     TRAINING_CSV
                     )
-                )     
+                )
+    """
     test_data = pd.read_csv(os.path.join(
                     os.path.dirname(os.getcwd()),
                     DATA_FOLDER,
                     TEST_CSV
                     )
                 )
-    print(training_data.head())
-    print(training_data.shape, test_data.shape)
-    return training_data, test_data
+    """    
+    
+    print(df.head())
+    print(df.shape)
+    return df
 
-def split_data(df: pd.DataFrame):
+def split_data(df: pd.DataFrame, test_size=0.2, random_state=42):
     """
     Desc
     """
     y = df["Price"]
     X = df.drop(columns="Price")
-    return (X, y)
+    X_train, y_train, X_test, y_test = train_test_split(
+        X,
+        y,
+        test_size=test_size,
+        random_state=random_state
+    )
+    
+    return (X_train, y_train, X_test, y_test)
 
-def remove_cols(df: pd.DataFrame):
+def remove_cols(X: pd.DataFrame):
     """
     Remove columns that were proved not useful for the study.
     """
-    df_identifiers = df[['Listing ID', 'Listing Name', 'Host ID']]
-    df.drop(columns=[
+    df_identifiers = X[['Listing ID', 'Listing Name', 'Host ID']]
+    X.drop(columns=[
         'Listing Name', 
         'City',
         'Country Code',
-        'Code',
+        'Country',
         'Listing ID',
         'Listing Name',
         'Host ID'
@@ -73,24 +85,43 @@ def remove_cols(df: pd.DataFrame):
     ) # those columns only take a single value
     
     # those columns are useful for identifying the samples
-    return (df, df_identifiers)
+    return (X, df_identifiers)
 
 def categorical_features_handler():
+    """
+    Apply one-hot encoding 
+    """
     return 0
 
 def numerical_features_scaler():
     return 0
 
-def preprocessing(df: pd.DataFrame):
+def preprocessing(X: pd.DataFrame):
     """
     Function applying all the previous preprocessing functions.
     """
-    (X, y) = split_data(df)
-    (X, df_identifiers) = remove_cols(X)
-    return (X, y, df_identifiers)
+    (X_clean, df_identifiers) = remove_cols(X)
+    
+    return (X_clean, df_identifiers)
 
 
 if __name__ == "__main__":
-    training_data, test_data = load_data()
-    (X_train, X_test, y_train, y_test) = split_data(training_data, test_data)
+    df = load_data() # full data
+    # print(df.shape)
+    (X_train, X_test, y_train, y_test) = split_data(df)
+    # print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+    X_train, df_train_identifiers = preprocessing(X_train)
+    X_test, df_test_identifiers = preprocessing(X_test)
+    # print(X_train.shape)
+    # print(X_test.shape)
+    
     print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+
+"""
+def scaler(method):
+    methods_available = ["MinMAx", "StandardScaler"]
+    assert(method in methods, message="")
+    
+    gerer method 
+        
+"""
