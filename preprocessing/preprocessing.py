bf273c647499ce6231f8d4e5d6f6be01ff4e489a
Clément Boulay <clement.boulay@student-cs.fr>
2022-10-14 10:01:56 +0200

    Add split_data, remove_cols and preprocessing function

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -4,6 +4,7 @@ Preprocessing.py
 
 # Python ≥3.5 is required
 import sys
+from tkinter import Y
 assert sys.version_info >= (3, 5)
 import os
 
@@ -22,6 +23,9 @@ import warnings
 warnings.filterwarnings(action="ignore", message="^internal gelsd")
 
 def load_data():
+    """
+    Desc
+    """
     DATA_FOLDER = "data/"
     TRAINING_CSV = "train_airbnb_berlin.csv"
     TEST_CSV = "test_airbnb_berlin.csv"
@@ -36,11 +40,57 @@ def load_data():
     test_data = pd.read_csv(os.path.join(
                     os.path.dirname(os.getcwd()),
                     DATA_FOLDER,
-                    TRAINING_CSV
+                    TEST_CSV
                     )
                 )
     print(training_data.head())
+    print(training_data.shape, test_data.shape)
     return training_data, test_data
+
+def split_data(df: pd.DataFrame):
+    """
+    Desc
+    """
+    y = df["Price"]
+    X = df.drop(columns="Price")
+    return (X, y)
+
+def remove_cols(df: pd.DataFrame):
+    """
+    Remove columns that were proved not useful for the study.
+    """
+    df_identifiers = df[['Listing ID', 'Listing Name', 'Host ID']]
+    df.drop(columns=[
+        'Listing Name', 
+        'City',
+        'Country Code',
+        'Code',
+        'Listing ID',
+        'Listing Name',
+        'Host ID'
+        ],
+        inplace=True
+    ) # those columns only take a single value
+    
+    # those columns are useful for identifying the samples
+    return (df, df_identifiers)
+
+def categorical_features_handler():
+    return 0
+
+def numerical_features_scaler():
+    return 0
+
+def preprocessing(df: pd.DataFrame):
+    """
+    Function applying all the previous preprocessing functions.
+    """
+    (X, y) = split_data(df)
+    (X, df_identifiers) = remove_cols(X)
+    return (X, y, df_identifiers)
+
 
 if __name__ == "__main__":
     training_data, test_data = load_data()
+    (X_train, X_test, y_train, y_test) = split_data(training_data, test_data)
+    print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
