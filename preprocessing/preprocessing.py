824d785eccf623a17131b9e50ff79d8bf402fe51
Clément Boulay <clement.boulay@student-cs.fr>
2022-10-22 16:04:45 +0200

    Add boolean features encoding, categorical features filling, customs orders in ordinal encoding, custom errors

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -45,18 +45,20 @@ CATEGORICAL_COLUMNS = [
     "Room Type",
     "Instant Bookable",
     "Business Travel Ready",
-    "Host Response Time",
+    "Host Response Time"
 ]
 
 NOMINAL_FEATURES = [
-    "Is Superhost",
     "neighbourhood",
     "Neighborhood Group",
+    "Property Type"
+]
+
+BOOLEAN_FEATURES = [ # apply only a binary map 0:1 for these (take only 2 values)
+    "Is Superhost",
     "Is Exact Location",
-    "Property Type",
     "Instant Bookable",
-    "Business Travel Ready",
-    
+    "Business Travel Ready"
 ]
 
 ORDINAL_FEATURES = [
@@ -65,20 +67,35 @@ ORDINAL_FEATURES = [
 ]
 
 NUMERICAL_FEATURES = [
-    "Postal Code",
     "Accomodates",
     "Bathrooms",
     "Beds",
     "Bedrooms",
     "Guests Included",
     "Min Nights",
+    "Reviews",
+    "Overall Rating",
+    "Accuracy Rating",
+    "Cleanliness Rating",
+    "Checkin Rating",
+    "Communication Rating",
+    "Location Rating",
+    "Value Rating",
+    "Host Response Rate"
+]
+
+PERCENTAGE_COLUMNS = [
+    "Host Response Rate"
+]
+
+HYBRID_FEATURES = [ # features that are neither really numerical nor really categorical
+    "Postal Code"
 ]
 
 UNIVALUED_COLUMNS = [ # these columns take only a single, therefore are not useful for predicting the target
-    "Country",
-    "Country Code",
-    "Country",
-    "City"
+    "Country", # only 'Germany'
+    "Country Code", # only 'DE
+    "City" # only 'nan' or '*' (replaced by np.nan) or 'Berlin'
 ]
 
 IDENTIFIERS_COLUMNS = [ # these columns are only useful for identifying the samples
@@ -91,12 +108,27 @@ IDENTIFIERS_COLUMNS = [ # these columns are only useful for identifying the samp
 # CUSTOM ERRORS
 
 class UnknownFillMethodError(Exception):
+    """
+    Desc.
+    """
     pass
 
 class UnknownEncodingMethodError(Exception):
+    """
+    Desc.
+    """
     pass
 
 class UnalignedDataFramesError(Exception):
+    """
+    Desc.
+    """
+    pass
+
+class UnspecifiedMappingError(Exception):
+    """
+    Desc.
+    """
     pass
 
 # AUXILIARY PREPROCESSING FUNCTIONS
@@ -126,6 +158,9 @@ def process_datetime_columns(df: pd.DataFrame):
 
 
 def datetime_to3columns(df: pd.DataFrame):
+    """
+    Desc.
+    """
     for col in DATETIME_COLUMNS:
         df[col + "_year"] = df[col].apply(lambda x: x.year)
         df[col + "_month"] = df[col].apply(lambda x: x.month)
@@ -133,7 +168,10 @@ def datetime_to3columns(df: pd.DataFrame):
     df.drop(columns=DATETIME_COLUMNS, inplace=True)
 
 
-def host_response_rate_transform(x):
+def percentage_cols_transform(x):
+    """
+    Desc.
+    """
     if type(x) == float:
         return x
     else:
@@ -141,19 +179,12 @@ def host_response_rate_transform(x):
         return float(x) / 100
 
 
-def handle_host_response_rate(df: pd.DataFrame):
+def handle_percentage_features(df: pd.DataFrame):
     """
     This column is special. It is numerical but involved percentages. We need to format it apart.
     """
-    df["Host Response Rate"] = df["Host Response Rate"].apply(host_response_rate_transform)
-    return df
-
-
-def ensure_numerical_columns_type(df):
-    """
-    Make sure all numerical columns are of type float.
-    """
-    df[NUMERICAL_FEATURES] = df[NUMERICAL_FEATURES].applymap(float)
+    for col in PERCENTAGE_COLUMNS:
+        df[col] = df[col].apply(percentage_cols_transform)
     return df
 
 
@@ -188,7 +219,10 @@ def fill_na_mean(X: pd.DataFrame):
     Desc.
     """
     for col in NUMERICAL_FEATURES:
-        mean = X[col].mean()
+        try:
+            mean = X[col].mean()
+        except TypeError: # column is encoded using strings
+            mean = np.mean(X[col].apply(lambda x: float(x)))
         X[col].apply(lambda x: mean if(pd.isna(x)) else x)
     return X
 
@@ -198,10 +232,31 @@ def fill_na_median(X: pd.DataFrame):
     Desc.
     """
     for col in NUMERICAL_FEATURES:
-        med = X[col].median()
+        try:
+            med = X[col].median()
+        except TypeError: # column is encoded using strings
+            med = np.median(X[col].apply(lambda x: float(x)))
         X[col].apply(lambda x: med if(pd.isna(x)) else x)
     return X
 
+
+def fill_categorical_features(X: pd.DataFrame):
+    """
+    Desc.
+    """
+    for col in CATEGORICAL_COLUMNS:
+        # apply custom treatments depending on columns
+        # TO DO
+        X[col] = X[col].apply(lambda x: np.argmax(X[col].value_counts()) if pd.isna(x) else x)
+
+    return X
+
+
+def scale_numerical_features(X: pd.DataFrame):
+    """
+    TO DO.
+    """
+    return 0
 
 def apply_one_hot_encoding(X: pd.DataFrame, full=False):
     """
@@ -226,7 +281,7 @@ def apply_one_hot_encoding(X: pd.DataFrame, full=False):
     return X
 
 
-def apply_label_encoding(X: pd.DataFrame, full=False):
+def apply_label_encoding(X: pd.DataFrame, full=False, use_sklearn=False):
     """
     Apply a Label encoding on the ordinal features. If full is set to True, apply the Label encoding onto all the
     categorical features.
@@ -243,20 +298,85 @@ def apply_label_encoding(X: pd.DataFrame, full=False):
     else:
         # apply the label encoding on ordinal features only
         cols_to_process = ORDINAL_FEATURES
+
     for col in cols_to_process:
         if col == "Host Response Time": # specify the order to use for ordinal encoding, since it means something
-           pass
+           mapping = {
+                "a few days or more": 0,
+                "within a day": 1,
+                "within a few hours" : 2,
+                "within an hour": 3
+           }
         elif col == "Room Type":
-            pass
+            mapping = {
+                "Shared room": 0,
+                "Private room": 1,
+                "Entire home/apt": 2
+            }
+        elif col == "Room Type":
+            mapping = {
+                "Shared room": 0,
+                "Private room": 1,
+                "Entire home/apt": 2
+            }
         else:
-            pass
+            mapping = None
 
-        encoder = OrdinalEncoder()
-        encoded_col = encoder.fit_transform(X[[col]])
-        X[[col]] = encoded_col.reshape(-1, 1)
+        if use_sklearn or (mapping is None): # beware! unable to specify the order of categories using OrdinalEncoder yet
+            encoder = OrdinalEncoder()
+            encoded_col = encoder.fit_transform(X[[col]])
+            X[col] = encoded_col.reshape(-1, 1)
+        else: # encode only the ordinal features using a specified mapping
+            X[col] = X[col].apply(lambda x: mapping[x] if x in mapping.keys() else np.nan)
 
     return X
     
+
+def encode_boolean_features(X: pd.DataFrame):
+    """
+    Encode boolean features using a binary map 0:1.
+    Note: binary maps can be viewed as a two-class ordinal encoding.
+    """
+    
+    for col in BOOLEAN_FEATURES:
+        if col == "Is Superhost": # specify the order to binary map
+           mapping = {
+                "f": 0,
+                "t": 1
+           }
+        elif col == "Is Exact Location":
+            mapping = {
+                "f": 0,
+                "t": 1
+            }
+        elif col == "Instant Bookable":
+            mapping = {
+                "f": 0,
+                "t": 1
+            }
+        elif col == "Business Travel Ready":
+            mapping = {
+                "f": 0,
+                "t": 1
+            }
+        else:
+            mapping = None
+
+        if mapping is None:
+            raise UnspecifiedMappingError
+        else:
+            X[col] = X[col].apply(lambda x: mapping[x] if x in mapping.keys() else np.nan)
+
+    return X
+
+
+def ensure_numerical_columns_type(X: pd.DataFrame):
+    """
+    Make sure all numerical columns are of type float.
+    """
+    X[NUMERICAL_FEATURES] = X[NUMERICAL_FEATURES].applymap(float)
+    return X
+
 
 def split_data(X, y, test_size=0.2, random_state=42):
     """
@@ -282,6 +402,9 @@ def preprocessing(df: pd.DataFrame,
     # then, remove samples with a missing y-value (missing target)
     remove_samples_with_missing_target(df)
 
+    # drop the columns that take only a single value
+    drop_univalued_columns(df)
+
     # then, convert columns involving dates to the proper format
     df = process_datetime_columns(df)
 
@@ -289,13 +412,7 @@ def preprocessing(df: pd.DataFrame,
     datetime_to3columns(df)
 
     # apply a special treatment to the host_response_rate column, which involves percentages
-    df = handle_host_response_rate(df)
-
-    # ensure all numerical columns are of type float
-    df = ensure_numerical_columns_type(df)
-
-    # drop the columns that take only a single value
-    drop_univalued_columns(df)
+    df = handle_percentage_features(df)
 
     # extract the identifiers from the data (columns needed to identify a given sample, like ID, Name...)
     (data, df_identifiers) = extract_identifiers(df)
@@ -310,6 +427,12 @@ def preprocessing(df: pd.DataFrame,
         X = fill_na_mean(X)
     elif fill_na_method == "median":
         X = fill_na_median(X)
+
+    # replace NaN values for the categorical columns too, using the majority value
+    X = fill_categorical_features(X)
+
+    # scale numerical features
+    #X = scale_numerical_features(X) # UNCOMMENT ONCE DONE
     
     # now, we apply encoding to the categorical features
     if encoder_method not in ["classical", "FullLabelEncoder", "FullOneHotEncoder"]:
@@ -321,8 +444,14 @@ def preprocessing(df: pd.DataFrame,
     elif encoder_method == "FullOneHotEncoder": # WARNING: be very careful when we use the following
         X = apply_one_hot_encoding(X, full=True)
     elif encoder_method == "FullLabelEncoder": # WARNING: be very careful when we use the following
-        X = apply_label_encoding(X, full=True)
+        X = apply_label_encoding(X, full=True, use_sklearn=True)
     
+    # last, encode the boolean features using a binary map
+    X = encode_boolean_features(X)
+
+    # ensure all numerical columns are of type float
+    X = ensure_numerical_columns_type(X)
+
     # last, we split the features and target into a training and a test set
     (X_train, X_test, y_train, y_test) = split_data(X, y)
 
@@ -336,6 +465,8 @@ if __name__ == "__main__":
     (X_train, X_test, y_train, y_test, df_identifiers) = preprocessing(df)
     # ensure everything went fine
     print(X_train.head())
+    # dump it into a csv for further inspection
+    X_train.to_csv('X_train_processed.csv', header=True, columns=X_train.columns)
 
 """
 TO DO:
